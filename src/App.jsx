42d2eb3a2690f01f43cd128ef9a2c8279024a651
Lukas <lukasdrinkwater97@gmail.com>
2023-09-17 20:26:08 +0100

    setup basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import React from "react";
-import { Link, NavLink, BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+// pages
 import Home from "./pages/home";
 import About from "./pages/about";
 import "./index.css";
+// layouts
+import RootLayout from "./layouts/RootLayout";
 
 // path in Route is the bath in the address bar
 // Link and NavLink show up as anchor tags in the DOM. The user sees them
 // NavLink automatically gets a class of "active" when its clicked
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet, NavLink } from "react-router-dom";
+// Header is at the top of every page. Then the other components are rendered
+// in <main>
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
